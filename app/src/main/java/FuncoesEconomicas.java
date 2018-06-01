ccbe064e34712e530ab3921b5f04b74c6de98871
andretomas1012524 <adtomas16@gmail.com>
2018-06-01 12:04:53 +0100

    Inicio do desenvolvimento do interface da aplicaçao.

---
+++ app/src/main/java/FuncoesEconomicas.java
@@ -0,0 +1,49 @@
+/**
+ * Created by Andre on 01-06-2018.
+ */
+
+public class FuncoesEconomicas {
+    private double a;
+    private double res;
+    private double preco;
+
+   //---------------- Declarar variaveis ---------------\\
+
+
+    private double getFA(){
+        return a;
+    }
+
+    private void setFA(double a) {
+        this.a = a;
+    }
+
+    private double getFres(){
+        return res;
+    }
+
+    private void setFres(double res){
+        this.res = res;
+    }
+
+    private double getPreco() {
+        return preco;
+    }
+
+    private void setPreco(double preco) {
+        this.preco = preco;
+    }
+    //------------- Funcoes e operacoes ------------\\
+
+
+   /* public double IVA(){
+        double a = getFA();
+        double res = getFres();
+        double preco = getPreco();
+
+        if(preco == 0){
+            res
+        }
+
+    }*/
+}
