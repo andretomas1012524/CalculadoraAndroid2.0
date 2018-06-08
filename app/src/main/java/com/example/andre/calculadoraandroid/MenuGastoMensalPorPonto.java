4e0f0892e9cff7c668442511760b4308430b967f
andretomas1012524 <adtomas16@gmail.com>
2018-06-08 12:45:15 +0100

    Novos Interfaces

---
+++ app/src/main/java/com/example/andre/calculadoraandroid/MenuGastoMensalPorPonto.java
@@ -0,0 +1,13 @@
+package com.example.andre.calculadoraandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuGastoMensalPorPonto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_gasto_mensal_por_ponto);
+    }
+}
