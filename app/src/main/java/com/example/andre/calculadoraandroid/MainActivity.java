f9b4fc50434186cd745f57d6950e05d6753c30bc
andretomas1012524 <adtomas16@gmail.com>
2018-06-13 10:48:51 +0100

    Menu de inserção de numeros básicos.Correção de erros ao abrir o menu binario.

--- app/src/main/java/com/example/andre/calculadoraandroid/MainActivity.java
+++ app/src/main/java/com/example/andre/calculadoraandroid/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void Cbinario(View view) {
-        Intent intent = new Intent(this,funcoesbinariascalculo.class);
+        Intent intent = new Intent(this,funcoescalculobinario.class);
         startActivity(intent);
     }
 
