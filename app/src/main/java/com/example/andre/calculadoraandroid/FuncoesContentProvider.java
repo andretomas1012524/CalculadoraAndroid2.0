d0347325f46b127436e22f372e80f6dce39d56d8
andretomas1012524 <adtomas16@gmail.com>
2018-06-13 16:57:02 +0100

    ContetnProvider Update e insert.

--- app/src/main/java/com/example/andre/calculadoraandroid/FuncoesContentProvider.java
+++ app/src/main/java/com/example/andre/calculadoraandroid/FuncoesContentProvider.java
@@ -2,7 +2,10 @@ package com.example.andre.calculadoraandroid;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -12,14 +15,53 @@ import android.support.annotation.Nullable;
  */
 
 public class FuncoesContentProvider extends ContentProvider {
+    public static final int Uri_Funcoes = 100;
+    public static final int Funcoes_ID = 101;
+    public static final int Pais_Uri = 200;
+    public static final int Pais_ID = 201;
+    DbEconomicaOpenHelper dbEconomicaOpenHelper;
+
+    private static UriMatcher getEconomiaUnimatcher(){
+        UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        uriMatcher.addURI("com.example.andre.calculadoraandroid","Funcoes", Uri_Funcoes);
+        uriMatcher.addURI("com.example.andre.calculadoraandroid","Funcoes/#", Funcoes_ID);
+        uriMatcher.addURI("com.example.andre.calculadoraandroid","Pais", Pais_Uri);
+        uriMatcher.addURI("com.example.andre.calculadoraandroid","Pais/#", Pais_ID);
+        return uriMatcher;
+
+    }
+
     @Override
     public boolean onCreate() {
-        return false;
+        dbEconomicaOpenHelper = new DbEconomicaOpenHelper(getContext());
+
+        return true;
     }
 
     @Nullable
     @Override
     public Cursor query(@NonNull Uri uri, @Nullable String[] strings, @Nullable String s, @Nullable String[] strings1, @Nullable String s1) {
+        SQLiteDatabase bd = dbEconomicaOpenHelper.getReadableDatabase();
+
+        String id = uri.getLastPathSegment();
+
+        UriMatcher matcher = getEconomiaUnimatcher();
+
+        switch (matcher.match(uri)){
+            case Uri_Funcoes:
+                return new DbTabelaFuncoes(bd).quarry(projecion,selection,selectionargs,null,null,sortOrder);
+                break;
+            case Funcoes_ID:
+                break;
+            case Pais_Uri:
+                break;
+            case Pais_ID:
+                break;
+            default:
+                throw new UnsupportedOperationException("Uri inválido !! :"+ uri);
+        }
+
+
         return null;
     }
 
@@ -32,7 +74,33 @@ public class FuncoesContentProvider extends ContentProvider {
     @Nullable
     @Override
     public Uri insert(@NonNull Uri uri, @Nullable ContentValues contentValues) {
-        return null;
+        SQLiteDatabase bd = dbEconomicaOpenHelper.getWritableDatabase();
+
+        long id;
+
+        UriMatcher matcher = getEconomiaUnimatcher();
+
+        switch (matcher.match(uri)){
+            case Uri_Funcoes:
+                id = new DbTabelaFuncoes(bd).insert(contentValues);
+                break;
+            case Pais_Uri:
+                 id = new DbTabelaPais(bd).insert(contentValues);
+                break;
+            default:
+                throw new UnsupportedOperationException("Uri inválido !! :"+ uri);
+        }
+        if (id>0){
+            NotifyChanges(uri);
+            return Uri.withAppendedPath(uri, Long.toString(id));
+        }else {
+            throw new SQLException("Não conseguiu inserir no registo");
+        }
+
+    }
+
+    private void NotifyChanges(@NonNull Uri uri) {
+        getContext().getContentResolver().notifyChange(uri,null);
     }
 
     @Override
