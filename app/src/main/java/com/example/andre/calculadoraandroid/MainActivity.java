2bb235c4d5edcf9f1238b399658004ec415c0d2d
andretomas1012524 <adtomas16@gmail.com>
2018-06-20 09:49:59 +0100

    Introdução do menu funcoescientificas

--- app/src/main/java/com/example/andre/calculadoraandroid/MainActivity.java
+++ app/src/main/java/com/example/andre/calculadoraandroid/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void Ccientifico(View view) {
-        Intent intent = new Intent(this,calculocientifico.class);
+        Intent intent = new Intent(this,funcoescientificas.class);
         startActivity(intent);
     }
 
---
+++ app/src/main/java/com/example/andre/calculadoraandroid/funcoescientificas.java
@@ -0,0 +1,29 @@
+package com.example.andre.calculadoraandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class funcoescientificas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_funcoescientificas);
+    }
+
+    public void scient(View view) {
+    }
+
+    public void subcient(View view) {
+    }
+
+    public void multcient(View view) {
+    }
+
+    public void divcient(View view) {
+    }
+
+    public void divIcient(View view) {
+    }
+}
