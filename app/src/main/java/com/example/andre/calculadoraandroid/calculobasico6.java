70ac0e8d6fac8b79330008099f0859bc94193efd
andretomas1012524 <adtomas16@gmail.com>
2018-06-27 13:46:10 +0100

    MenuPotencia Funcional.

--- app/src/main/java/com/example/andre/calculadoraandroid/calculobasico6.java
+++ app/src/main/java/com/example/andre/calculadoraandroid/calculobasico6.java
@@ -1,13 +1,66 @@
 package com.example.andre.calculadoraandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class calculobasico6 extends AppCompatActivity {
-
+    public static final double numMax = 999999999.99;
+    public static final double numMin = -999999999.99;
+    public static final String Numerobasico1 = "Numero1";
+    public static final String Numerobasico2 = "Numero2";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculobasico6);
+    }
+
+    public void calcularbasicopotn(View view) {
+        Intent intent = new Intent(this,Menuresultado7.class);
+
+        EditText editTextnum1 = (EditText)findViewById(R.id.nbasico10);
+        EditText editTextnum2 = (EditText)findViewById(R.id.nbasico11);
+
+        double num1;
+        double num2;
+
+        try {
+            num1 = Double.parseDouble(editTextnum1.getText().toString());
+        }catch (NumberFormatException e){
+            editTextnum1.setError("Numero 1 Invalido!! Porfavor preencha.");
+            editTextnum1.requestFocus();
+            return;
+        }
+        try {
+            num2 = Double.parseDouble(editTextnum2.getText().toString());
+        }catch (NumberFormatException e){
+            editTextnum2.setError("Numero 2 Invalido!! Porfavor preencha.");
+            editTextnum2.requestFocus();
+            return;
+        }
+
+        if(num1>numMax){
+            editTextnum1.setError("Numero 1 Invalido!! O limite máximo é 999999999.99");
+            editTextnum1.requestFocus();
+            return;
+        } else if(num1<numMin){
+            editTextnum1.setError("Numero 1 Invalido !! O limite mínimo é -999999999.99");
+            editTextnum1.requestFocus();
+            return;
+        }
+        if(num2>numMax){
+            editTextnum2.setError("Numero 2 Invalido!! O limite máximo é 999999999.99");
+            editTextnum2.requestFocus();
+            return;
+        } else if(num2<numMin){
+            editTextnum2.setError("Numero 2 Invalido !! O limite mínimo é -999999999.99");
+            editTextnum2.requestFocus();
+            return;
+        }
+        intent.putExtra(Numerobasico1,num1);
+        intent.putExtra(Numerobasico2,num2);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/andre/calculadoraandroid/maisfuncoesbasica.java
+++ app/src/main/java/com/example/andre/calculadoraandroid/maisfuncoesbasica.java
@@ -1,7 +1,9 @@
 package com.example.andre.calculadoraandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class maisfuncoesbasica extends AppCompatActivity {
 
@@ -9,5 +11,25 @@ public class maisfuncoesbasica extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maisfuncoesbasica);
+    }
+
+    public void potcal(View view) {
+        Intent intent = new Intent(this,calculobasico6.class);
+        startActivity(intent);
+    }
+
+    public void raizcal(View view) {
+        Intent intent = new Intent(this,calculobasico7.class);
+        startActivity(intent);
+    }
+
+    public void raizpredcal(View view) {
+        Intent intent = new Intent(this,calculobasico8.class);
+        startActivity(intent);
+    }
+
+    public void factcal(View view) {
+        Intent intent = new Intent(this,calculobasico9.class);
+        startActivity(intent);
     }
 }
